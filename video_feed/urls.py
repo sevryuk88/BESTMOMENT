7d0e804acec5c46453ead5d226cfa8af5fe06a24
Sevryuk <hakisds@gmail.com>
2025-05-17 14:22:00 +0300

    Add temporary migration and admin creation routes

--- video_feed/urls.py
+++ video_feed/urls.py
@@ -4,6 +4,26 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+from django.http import HttpResponse
+from django.core.management import call_command
+from django.contrib.auth import get_user_model
+
+# Временная функция для применения миграций
+def run_migrations(request):
+    call_command("migrate")
+    return HttpResponse("✅ Migrations applied.")
+
+# Временная функция для создания суперпользователя
+def create_admin_user(request):
+    User = get_user_model()
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("admin", "admin@example.com", "adminpass123")
+        return HttpResponse("✅ Superuser created.")
+    else:
+        return HttpResponse("⚠️ Superuser already exists.")
+
+
 urlpatterns = [
     #path('grappelli/', include('grappelli.urls')),
   
@@ -12,6 +32,10 @@ urlpatterns = [
     path('users/', include('users.urls', namespace="users")),  
     path('accounts/', include('django.contrib.auth.urls')),  # Встроенные маршруты Django
     path('webpush/', include('pushapp.urls', namespace='webpush')),
+    
+    path('run-migrations/', run_migrations),     # временный путь
+    path('create-admin/', create_admin_user),    # временный путь
+    
     
 ]
 
