96632d292eb9489959cbfff7fb746fea4cbdd65c
Sevryuk <hakisds@gmail.com>
2025-05-17 14:59:25 +0300

    Remove old DB_NAME config and use DATABASE_URL

--- video_feed/settings/base.py
+++ video_feed/settings/base.py
@@ -25,18 +25,12 @@ WEBPUSH_SETTINGS = {
     "VAPID_ADMIN_EMAIL": "admin@yourdomain.com"
 }
 
-
-# База данных
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST', default='localhost'),
-        'PORT': env('DB_PORT', default='5432'),
-    }
+    'default': env.db("DATABASE_URL")
 }
+
+
+
 
 # Email
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
